fd26436bb5007dc68c0996150ef62e15a4d9ccb5
Wilmer Mendoza <wmendozar.cief.pow@gmail.com>
2025-06-27 13:49:58 +0200

    inicio proyeco ducky

---
+++ Ducky_Ways/Ducky_Ways/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Ducky_Ways project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ducky_Ways.settings")
+
+application = get_wsgi_application()
